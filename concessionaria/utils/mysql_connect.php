6aa219ae9eafe59fe310e5dd9a08638e7e594ef1
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-01-08 23:40:15 -0300

    feat: completed conection

--- concessionaria/utils/mysql_connect.php
+++ concessionaria/utils/mysql_connect.php
@@ -5,18 +5,21 @@ $db   = "concessionaria";
 $user = "root";
 $pass = "password";
 // conecta ao banco de dados
-$con = mysqli_connect($host, $user, $pass, $db) or trigger_error(mysql_error(),E_USER_ERROR);
+$con = mysqli_connect($host, $user, $pass, $db) or trigger_error(mysqli_error(),E_USER_ERROR);
 // seleciona a base de dados em que vamos trabalhar
-print "Conectou!!";
-// mysqli_select_db($db, $con);
+// print "Conectou!!";
 // cria a instrução SQL que vai selecionar os dados
-$query = sprintf("SELECT identificador, nome, telefone FROM cadastro");
+
+$query = sprintf("SELECT * FROM carros"); //Não retorna nada pq o banco ta vazio
+
 // executa a query
 
-$dados = mysqli_query($con, $query) or die(mysqli_error());
+$dados = mysqli_query($con, $query) or die(mysqli_error($con));
 // // transforma os dados em um array
-// $linha = mysql_fetch_assoc($dados);
+$linha = mysqli_fetch_assoc($dados);
 // // calcula quantos dados retornaram
-// $total = mysql_num_rows($dados);
+$total = mysqli_num_rows($dados);
+
+print($total); // Fazer um for com o total para iterar na tela de listagem
 
 ?>
