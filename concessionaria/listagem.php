317c36933bb844dec926ac82a143aa4bcaf01ee5
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-01-19 08:53:34 -0300

    perf: adjusti list

--- concessionaria/listagem.php
+++ concessionaria/listagem.php
@@ -1,95 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Concessionária - Gerenciamento de Veículos</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container my-3 ">
-        <?php 
-            include "cabeçalho.php";
-        ?>
-
-        <?php 
-            include "navbar.php";
-        ?>
-
-        <?php 
-            include "./utils/mysql_connect.php";
-        ?>
+        <?php include "cabeçalho.php"; ?>
+        <?php include "navbar.php"; ?>
+        <?php include "./utils/mysql_connect.php"; ?>
 
         <section>
             <header class="card-header p-3 mb-3">
                 <h2>Veja os nossos veículos</h2>
             </header>
             <?php
-                if(!isset($_POST['id']) && !empty($_POST['id'])) {
-                    $idDelete = $_POST['id'];
-                    $queryDelete = sprintf("DELETE FROM carros WHERE id=$idDelete");
-                    $removed = mysqli_query($con, $queryDelete) or die(mysqli_error($con));
-                    print($removed);
-                    return;
-                }
-              
-                $query = sprintf("SELECT * FROM carros"); //Não retorna nada pq o banco ta vazio
-                $query2 = sprintf("INSERT INTO carros (id, marca, modelo, ano, preco, foto, cor, descricao) VALUES ('3','Chevrolet', 'Onix', 2018, 45000.00, 'teste.png', 'Branco', 'Muito top')");
-                //$dados = mysqli_query($con, $query2) or die(mysqli_error($con));
-                $dados = mysqli_query($con, $query) or die(mysqli_error($con));
-                // // transforma os dados em um array
-                // // calcula quantos dados retornaram
-                $total = mysqli_num_rows($dados);
-                
-                for($i = 0; $i < $total; $i++){
-                    $linha = mysqli_fetch_assoc($dados);
-                    // echo '<pre>'; print_r($linha); echo '</pre>';
-                    $id=$linha['id'];
-                    $marca=$linha['marca'];
-                    $preco=$linha['preco'];
-                    $cor=$linha['cor'];
-                    $descricao=$linha['descricao'];
-                    $modelo=$linha['modelo'];
-                    $foto=$linha['foto'];
-                    $ano=$linha['ano'];
-                print <<<END
-                    <div class="card p-2 my-3">
-                    <div class="row">
-                    <div class="col-md-3">
-                        <img src="https://placehold.it/400x400" class="img-thumbnail img-fluid">
-                    </div>
-                        <div class="col-md-9 p-3">
-                            <h3>$modelo</h3>
-                            <p>
-                                <strong>Marca:</strong> $marca <br>
-                                <strong>Ano:</strong> $ano <br>
-                                <strong>Preço:</strong> R$ $preco <br>
-                                <strong>Cor:</strong> $cor
-                            </p>
-                            <p>
-                                $descricao
-                            </p>
-                            <form action="editar.php" class="container" method="post">
-                                <input id="$id" name="id" type="hidden" value="$id">
-                                <button type="submit" class="btn btn-primary">Editar</button>
-                            </form>
-                            <form action="delete.php" class="container" method="post">
-                                <input id="$id" name="id" type="hidden" value="$id">
-                                <button type="submit" class="btn btn-danger">Excluir</button>
-                            </form>
-                        </div>
-                        </div>
-                        </div>
+            if (!isset($_POST['id']) && !empty($_POST['id'])) {
+                $idDelete = $_POST['id'];
+                $queryDelete = sprintf("DELETE FROM carros WHERE id=$idDelete");
+                $removed = mysqli_query($con, $queryDelete) or die(mysqli_error($con));
+                print($removed);
+                return;
+            }
 
-                    END;
-                    }
-                ?>
-                
+            $query = sprintf("SELECT * FROM carros"); //Não retorna nada pq o banco ta vazio
+            $query2 = sprintf("INSERT INTO carros (id, marca, modelo, ano, preco, foto, cor, descricao) VALUES ('3','Chevrolet', 'Onix', 2018, 45000.00, 'teste.png', 'Branco', 'Muito top')");
+            //$dados = mysqli_query($con, $query2) or die(mysqli_error($con));
+            $dados = mysqli_query($con, $query) or die(mysqli_error($con));
+            $total = mysqli_num_rows($dados);
+            ?>
+            <?php
+
+            for ($i = 0; $i < $total; $i++) {
+                $linha = mysqli_fetch_assoc($dados);
+                echo '<pre>';
+                print_r($linha);
+                echo '</pre>';
+                $id = $linha['id'];
+                $marca = $linha['marca'];
+                $preco = $linha['preco'];
+                $cor = $linha['cor'];
+                $descricao = $linha['descricao'];
+                $modelo = $linha['modelo'];
+                $foto = $linha['foto'];
+                $ano = $linha['ano'];
+            ?>
+                <div class="card p-2 my-3">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="https://placehold.it/400x400" class="img-thumbnail img-fluid">
+                        </div>
+                        <div class="col-md-9 p-3">
+                            <h3><?php print($modelo) ?></h3>
+                            <p>
+                                <strong>Marca:</strong> <?php print($marca); ?><br>
+                                <strong>Ano:</strong> <?php print($ano); ?><br>
+                                <strong>Preço:</strong> R$<?php echo number_format($preco, 2, ",", "."); ?><br>
+                                <strong>Cor:</strong> <?php print($cor); ?>
+                            </p>
+                            <p>
+                                <?php print($descricao); ?>
+                            </p>
+                            <div class="row justify-content-end">
+                                <div class="col-xs">
+                                    <form action="editar.php" class="container" method="post">
+                                        <input id="<?php print($id); ?>" name="id" type="hidden" value="<?php print($id); ?>">
+                                        <button type="submit" class="btn btn-primary">Editar</button>
+                                    </form>
+                                </div>
+                                <div class="col-xs">
+                                    <form action="delete.php" class="container" method="post">
+                                        <input id="<?php print($id); ?>" name="id" type="hidden" value="<?php print($id); ?>">
+                                        <button type="submit" class="btn btn-danger">Excluir</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+
         </section>
 
         <hr>
         <?php include "footer.php" ?>
-    
-    
+
+
 </body>
+
 </html>
